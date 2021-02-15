c1ce92247bcb5acd1fddc2c6ec80d3ea40b82f6d
codebr3ak <barakaralph@gmail.com>
2021-02-15 14:30:41 +0300

    import random and string to generate password randomly in credentials.py

--- credentials.py
+++ credentials.py
@@ -1,3 +1,14 @@
+from user import User
+import string
+import random
+
+
+class Credentials:
+    """
+    Class to create and store user credentials
+    """
+
+    def __init__(self, first_name)
 class Credentials:
     """
     Class to store user credentials
