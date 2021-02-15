fc825e0d090d65f3837cc471dbff49c6759b187d
codebr3ak <barakaralph@gmail.com>
2021-02-15 15:36:05 +0300

    created a test classes and imported module and unittest module

---
+++ credentials_test.py
@@ -0,0 +1,15 @@
+import unittest
+import credentials
+
+
+class TestCredentials(unittest.TestCase):
+    """
+    Test class that defines test cases for Credentials class behaviours
+    Args:
+        unittest.TestCase: TestCase class that helps in creates test cases
+    """
+    def test_
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ user_test.py
@@ -0,0 +1,15 @@
+import unittest
+import user
+
+
+class TestUser(unittest.TestCase):
+    """
+    Test class that defines test cases for User class behaviours
+    Args:
+        unittest.TestCase: TestCase class that helps in creates test cases
+    """
+    def test_
+
+
+if __name__ == '__main__':
+    unittest.main()
